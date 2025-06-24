37a3b9e6984f2977bcfee6c6e5704dab33cafe91
Vishnu-Gopalsamy <vishnugopalsamy2005@gmail.com>
2025-06-24 08:57:42 +0530

    day6

--- DAY6/backend/server.js
+++ DAY6/backend/server.js
@@ -1,21 +1,33 @@
 import express from 'express';
-import connectDB from './config/db.js';
-
+import mongoose from 'mongoose';
+import { connectDB } from './config/db.js'; 
+import user from './model/user.js'; // Assuming you have a user model defined
 const app = express();
-connectDB();
-app.get('/', (req, res) => {
-  res.send('from get request');
-});
-app.post('/', (req, res) => {
-  res.send('from post request');
-});
-app.put('/', (req, res) => {
-  res.send('from put request');
-});
-app.delete('/', (req, res) => {
-  res.send('from delete request');
+
+
+connectDB()
+
+const userSchema = new mongoose.Schema({
+    name: {type :String, required: true},
+    age:Number
 });
 
-  app.listen(3000, () => {
-    console.log('Server is running on http://localhost:3000');
-  });
+// Prevent OverwriteModelError
+const User = mongoose.models.User || mongoose.model('User', userSchema);
+
+app.get('/get', (req, res) => {
+    res.send('Hello, World!');
+});
+app.post('/post', (req, res) => {
+    res.send('Post request received!');
+});
+app.put('/put', (req, res) => {
+    res.send('Put request received!');
+});
+app.delete('/delete', (req, res) => {
+    res.send('Delete request received!');   
+});
+
+app.listen(3000, () => {
+    console.log('Server is running on 3000');
+});
